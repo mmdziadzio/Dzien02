f96abcf5e49ab40ecf5ee47d3d695ac008e47844
mmdziadzio@gmail.com <mmdziadzio@gmail.com>
2023-04-16 15:12:47 +0200

    Add project files.

---
+++ Serializacja/BinarySerializer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacja
+{
+    internal class BinarySerializer
+    {
+        public static void Create()
+        {
+            Employee emp = new Employee()
+            {
+                Id = 123,
+                FirstName = "Jan",
+                LastName = "Kowalski",
+                AccessRooms = new List<int> { 2, 3, 4 },
+                IsMenager = false,
+                StartAt = new DateTime(2022, 1, 1)
+            };
+            emp.SetToken(Guid.NewGuid().ToString());
+
+            // Serializacja (binarna)
+            using (FileStream fs = new FileStream("dump.bin",FileMode.Create))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, emp);
+            }
+
+            // Deserializacja (binarna)
+            using (FileStream fs = new FileStream("dump.bin", FileMode.Open))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                Employee empDeserial = bf.Deserialize(fs) as Employee;
+                if (empDeserial != null)
+                {
+                    Console.WriteLine(empDeserial);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Serializacja/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacja
+{
+    [Serializable]
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsMenager { get; set; }
+        public List<int> AccessRooms { get; set; }
+        public List<string> ExtraData { get; set; } 
+        public DateTime? StartAt { get; set; }
+
+        private string Token;
+
+        public void SetToken(string token)
+        {
+            Token = token;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+    }
+}
